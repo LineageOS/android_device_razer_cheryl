504fc8305693f817d4ce8745f515fef6a9eaf6a6
David Lin <dtwlin@google.com>
2017-04-10 14:51:38 -0700

    cheryl: enable audio rotation feature
    Bug: 34897462
    
    Signed-off-by: David Lin <dtwlin@google.com>
    
    Change-Id: Ia8d2abbfe3d0ba79c5cf1791f23ec3395842a2a9

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -28,6 +28,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.bt.a2dp_offload_cap=sbc-aac \
     persist.vendor.bt.aac_frm_ctl.enabled=true \
     ro.af.client_heap_size_kbyte=7168 \
+    ro.audio.monitorRotation=true \
     ro.config.alarm_vol_default=10 \
     ro.config.alarm_vol_steps=16 \
     ro.config.media_vol_default=7 \
