eadd74a56998f455559e96d91679f01122040a83
Dan Cashman <dcashman@google.com>
2017-06-01 15:25:22 -0700

    cheryl: Add BOARD_PLAT_[PUBLIC|PRIVATE]_SEPOLICY_DIR
    Move vendor policy to vendor and add a place for system extensions.
    Also add such an extension: a labeling of the qti.ims.ext service.
    
    Bug: 38151691
    Bug: 62041272
    Test: Policy binary identical before and after, except plat_service_contexts
    has new service added.
    Change-Id: Ie4e8527649787dcf2391b326daa80cf1c9bd9d2f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,7 +146,9 @@ VENDOR_SECURITY_PATCH := 2018-07-05
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
