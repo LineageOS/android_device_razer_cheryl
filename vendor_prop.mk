95c0d361b482542c636192a384f33fe2b3b0327f
Jorim Jaggi <jjaggi@google.com>
2018-03-27 15:57:47 +0200

    cheryl: Disable sdm rotator downscaler
    Rotator down scaler is causing additional delays that are better
    handled by GL composition.
    
    Test: Open app, no jank
    Bug: 73640835
    Change-Id: I6ea9baa28acc222afcf365f3bf107149bc8974d9

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -78,6 +78,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qualcomm.cabl=0 \
     ro.sf.lcd_density=560 \
     vendor.display.disable_partial_split=1 \
+    vendor.display.disable_rotator_downscale=1 \
     vendor.display.disable_skip_validate=1 \
     vendor.display.enable_default_color_mode=1 \
     vendor.display.perf_hint_window=50 \
