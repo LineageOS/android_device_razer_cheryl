6a1b0dd504adc328c1abc6d9261abcb8cc29df43
LuK1337 <priv.luk@gmail.com>
2018-08-26 15:10:38 +0200

    cheryl: Set vendor security patch level using VENDOR_SECURITY_PATCH
    Change-Id: Id7e80418c2437132bad3fa404b8a966d64758a06

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -192,6 +192,9 @@ TARGET_RIL_VARIANT := caf
 # Root
 BOARD_ROOT_EXTRA_FOLDERS := bt_firmware dsp firmware persist
 
+# Security patch level
+VENDOR_SECURITY_PATCH := 2018-07-05
+
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
