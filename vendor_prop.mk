e7c7ed9c411f7e2eae9dd005d78c83c41336a0b6
Vol Zhdanov <wight554@gmail.com>
2018-09-23 07:48:08 +0000

    cheryl: use new hwcomposer properties
    Change-Id: Ia2a494aa1a1fb2773c360cc27c2c78cea213f72d

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -70,8 +70,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.egl.force_msaa=false \
-    debug.gralloc.enable_fb_ubwc=1 \
-    debug.gralloc.gfx_ubwc_disable=0 \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=0 \
@@ -79,7 +77,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610 \
     ro.qualcomm.cabl=0 \
     ro.sf.lcd_density=560 \
-    vendor.display.enable_default_color_mode=1
+    vendor.display.disable_skip_validate=1 \
+    vendor.display.enable_default_color_mode=1 \
+    vendor.display.perf_hint_window=50 \
+    vendor.gralloc.enable_fb_ubwc=1
 
 # DPM
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -131,9 +132,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
     ro.vendor.qti.core_ctl_max_cpu=4 \
-    ro.vendor.qti.core_ctl_min_cpu=2 \
-    sdm.debug.disable_skip_validate=1 \
-    sdm.perf_hint_window=50
+    ro.vendor.qti.core_ctl_min_cpu=2
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
