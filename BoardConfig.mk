4506fa813e38f6517ae1736ad6d680cf026f8831
Nicolas Geoffray <ngeoffray@google.com>
2019-02-22 15:38:22 +0000

    cheryl: Generate the apex image.
    Change-Id: I18a76766590db27a4c32534dc948f55f78da166c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -63,6 +63,9 @@ BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
 # Assert
 TARGET_OTA_ASSERT_DEVICE := cheryl
 
+# APEX image
+DEXPREOPT_GENERATE_APEX_IMAGE := true
+
 # Audio
 BOARD_USES_ALSA_AUDIO := true
 
