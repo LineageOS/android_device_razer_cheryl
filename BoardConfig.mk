ee9bb365a6da390d6f32d0ec1c3ea2ce5df165d1
Tao Bao <tbao@google.com>
2019-05-06 22:42:02 -0700

    cheryl: AB_OTA_UPDATER is BoardConfig variable.
    Bug: 130433003
    Test: TreeHugger
    Change-Id: I70c7a884df45781e8b14339edd02985ba3de02fd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,6 +59,14 @@ BOARD_KERNEL_CMDLINE += loop.max_part=7
 # Platform
 TARGET_BOARD_PLATFORM := msm8998
 
+# A/B
+AB_OTA_UPDATER := true
+
+AB_OTA_PARTITIONS += \
+    boot \
+    system \
+    vendor
+
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
 
