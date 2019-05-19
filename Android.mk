7d514eb19221f0393e9dc01bc6f64459cbe25000
Michael Bestas <mkbestas@lineageos.org>
2019-05-19 16:13:41 +0300

    cheryl: Create fih_rfs symlink
    * Required by tftp_server
    
    Change-Id: I5d3e8a8586227e3ac05fd3a83c6cb052ef718e44

--- Android.mk
+++ Android.mk
@@ -49,12 +49,14 @@ $(RFS_MSM_MPSS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Creating RFS MSM MPSS folder structure: $@"
 	@rm -rf $@/*
 	@mkdir -p $(dir $@)/readonly/vendor
+	@mkdir -p $(dir $@)/fih_rfs/data
 	$(hide) ln -sf /data/vendor/tombstones/rfs/modem $@/ramdumps
 	$(hide) ln -sf /persist/rfs/msm/mpss $@/readwrite
 	$(hide) ln -sf /persist/rfs/shared $@/shared
 	$(hide) ln -sf /persist/hlos_rfs/shared $@/hlos
 	$(hide) ln -sf /firmware $@/readonly/firmware
 	$(hide) ln -sf /vendor/firmware $@/readonly/vendor/firmware
+	$(hide) ln -sf /data/misc $@/fih_rfs/data/misc
 
 RFS_MSM_SLPI_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/msm/slpi/
 $(RFS_MSM_SLPI_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
