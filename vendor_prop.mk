d115528a1daf35fa3eef49981adc8c44de93d21d
Michael Bestas <mkbestas@lineageos.org>
2019-05-19 17:06:53 +0300

    cheryl: Update props from 6033
    Change-Id: I8a649ed31b57e1a7a327fdc06e00d0cf7bb5e0b0

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,86 +16,70 @@
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-    aaudio.hw_burst_min_usec=2000 \
-    aaudio.mmap_exclusive_policy=2 \
-    aaudio.mmap_policy=2 \
-    af.fast_track_multiplier=2 \
-    audio.adm.buffering.ms=6 \
+    af.fast_track_multiplier=1 \
     audio.deep_buffer.media=true \
-    audio.offload.min.duration.secs=15 \
     audio.offload.video=true \
-    persist.audio.dirac.speaker=true \
-    persist.audio.speaker.dualmode=true \
+    audio.safemedia.force=true \
+    persist.vendor.audio.fluence.audiorec=false \
     persist.vendor.audio.fluence.speaker=true \
     persist.vendor.audio.fluence.voicecall=true \
     persist.vendor.audio.fluence.voicerec=false \
     persist.vendor.audio.ras.enabled=false \
-    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxhd-aac \
-    ro.config.vc_call_vol_steps=11 \
+    persist.vendor.bt.a2dp_offload_cap=sbc-aac \
     ro.vendor.audio.sdk.fluencetype=fluence \
     ro.vendor.audio.sdk.ssr=false \
     vendor.audio.dolby.ds2.enabled=false \
     vendor.audio.dolby.ds2.hardbypass=false \
     vendor.audio.flac.sw.decoder.24bit=true \
-    vendor.audio.hw.aac.encoder=false \
+    vendor.audio.hw.aac.encoder=true \
     vendor.audio.noisy.broadcast.delay=600 \
     vendor.audio.offload.buffer.size.kb=32 \
     vendor.audio.offload.gapless.enabled=true \
     vendor.audio.offload.multiaac.enable=true \
-    vendor.audio.offload.multiple.enabled=true \
+    vendor.audio.offload.multiple.enabled=false \
     vendor.audio.offload.passthrough=false \
     vendor.audio.offload.pstimeout.secs=3 \
-    vendor.audio.offload.track.enable=false \
-    vendor.audio.parser.ip.buffer.size=0 \
+    vendor.audio.offload.track.enable=true \
+    vendor.audio.parser.ip.buffer.size=262144 \
     vendor.audio.safx.pbe.enabled=true \
     vendor.audio.tunnel.encode=false \
     vendor.audio.use.sw.alac.decoder=true \
     vendor.audio.use.sw.ape.decoder=true \
-    vendor.audio_hal.period_size=192
+    vendor.audio_hal.period_size=192 \
+    vendor.fm.a2dp.conc.disabled=true
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     qcom.bluetooth.soc=cherokee \
-    ro.bluetooth.hfp.ver=1.6
+    ro.bt.bdaddr_path=/proc/bt_mac
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.HAL3.enabled=1 \
-    persist.camera.set.afd=4 \
-    persist.camera.xm.green.b=0.96 \
-    persist.camera.xm.green.r=0.97 \
-    persist.dualcam.lpm.enable=1 \
-    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.miui.cit,org.lineageos.snap \
+    persist.camera.gyro.android=2 \
+    persist.camera.gyro.disable=0 \
+    vendor.camera.aux.packagelist=com.razerzone.camera \
     vidc.enc.dcvs.extra-buff-count=2
-
-# CNE
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.cne.feature=1
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.gralloc.enable_fb_ubwc=1 \
-    debug.sf.enable_hwc_vds=1 \
-    debug.sf.hw=1 \
-    debug.sf.latch_unsignaled=1 \
+    debug.sf.latch_unsignaled=0 \
+    persist.metadata_dynfps.disable \
     ro.opengles.version=196610 \
-    ro.sf.lcd_density=480 \
-    vendor.display.enable_default_color_mode=1
-
-# DPM
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.dpm.feature=1 \
-    persist.vendor.dpm.nsrm.bkg.evt=3955
+    ro.sf.lcd_density=560
 
 # FRP
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.frp.pst=/dev/block/platform/soc/1da4000.ufshc/by-name/frp
 
-# IMS
+# Keys
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.dbg.volte_avail_ovr=1 \
-    persist.dbg.vt_avail_ovr=1 \
-    persist.vendor.qti.telephony.vt_cam_interface=1
+    qemu.hw.mainkeys=0
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.vidc.dec.120fps.enabled=1 \
+    vendor.vidc.dec.drc.enable=1 \
+    vidc.enc.dcvs.extra-buff-count=2
 
 # Memory
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -106,77 +90,44 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=16m \
     dalvik.vm.heaptargetutilization=0.75
 
-# Network manager
+# NFC
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.data.netmgrd.qos.enable=true \
-    persist.data.wda.enable=true \
-    persist.data.df.agg.dl_pkt=10 \
-    persist.data.df.agg.dl_size=4096 \
-    persist.data.df.dev_name=rmnet_usb0 \
-    persist.data.df.dl_mode=5 \
-    persist.data.df.iwlan_mux=9 \
-    persist.data.df.mux_count=8 \
-    persist.data.df.ul_mode=5
+    nfc.app_log_level=2
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
-    ro.vendor.qti.core_ctl_max_cpu=4 \
-    ro.vendor.qti.core_ctl_min_cpu=0 \
-    ro.vendor.qti.sys.fw.bg_apps_limit=60 \
-    sdm.debug.disable_skip_validate=1 \
-    sdm.perf_hint_window=50
+    sdm.debug.disable_skip_validate=1
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.mode=concurrent \
     persist.radio.apm_sim_not_pwdn=1 \
-    persist.radio.dynamic_sar=false \
-    persist.radio.multisim.config=dsds \
-    persist.rmnet.data.enable=true \
     persist.vendor.radio.custom_ecc=1 \
-    persist.vendor.radio.force_on_dc=true \
     persist.vendor.radio.rat_on=combine \
-    persist.vendor.radio.redir_party_num=1 \
-    persist.vendor.radio.report_codec=1 \
     persist.vendor.radio.sib16_support=1 \
     rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-    ro.telephony.default_cdma_sub=0 \
-    ro.telephony.default_network=22,22 \
-    ro.use_data_netmgrd=true \
-    telephony.lteOnCdmaDevice=1 \
-    vendor.voice.path.for.pcm.voip=true
-
-# Sensors
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.sensors.cfos=false \
-    persist.sys.sensors.pug=false \
-    persist.sys.sensors.spg=false \
-    ro.qti.sensors.taptap_gesture=true \
-    ro.vendor.sdk.sensors.gestures=false \
-    ro.vendor.sensors.cmc=false \
-    ro.vendor.sensors.dev_ori=false \
-    ro.vendor.sensors.dpc=true \
-    ro.vendor.sensors.facing=false \
-    ro.vendor.sensors.iod=false \
-    ro.vendor.sensors.mot_detect=true \
-    ro.vendor.sensors.multishake=true \
-    ro.vendor.sensors.pmd=false \
-    ro.vendor.sensors.sta_detect=true
-
-# Time services
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.delta_time.enable=true
+    vendor.voice.path.for.pcm.voip=false
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.security_patch=2018-07-05
 
-# WFD
+# Random
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.debug.wfd.enable=1 \
-    persist.sys.wfd.virtual=0
-
-# Wlan
-PRODUCT_PROPERTY_OVERRIDES += \
-    wifi.interface=wlan0
+    persist.mwc.walk_around=1 \
+    ro.oem_unlock_supported=true \
+    ro.build.shutdown_timeout=0 \
+    ro.com.google.gmsversion=8.1_201807 \
+    ro.control_privapp_permissions=enforce \
+    ro.cp_system_other_odex=1 \
+    ro.opa.eligible_device=true \
+    ro.qcom.hdr.config=/system/etc/hdr_tm_config.xml \
+    ro.razer.internal.api=1 \
+    ro.razer.internal.list=9 \
+    ro.razer.internal.mask=254 \
+    ro.razer.internal.zval=26 \
+    ro.setupwizard.mode=OPTIONAL \
+    ro.setupwizard.require_network=junk \
+    ro.setupwizard.rotation_locked=true \
+    sys.vendor.shutdown.waittime=500
