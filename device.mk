c915d94d77fb2d4b7d7d3965e0ac71121746c338
Michael Bestas <mkbestas@lineageos.org>
2019-05-20 21:55:52 +0300

    cheryl: Remove GPS HAL sources
    * We are going to use prebuilt GPS for now
    
    Change-Id: I0bb8ca6117a175d4b40a3ff8f5b07cd9b38bbeae

--- device.mk
+++ device.mk
@@ -214,14 +214,6 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service
 
 # GPS
-PRODUCT_PACKAGES += \
-    libgnss \
-    libgnsspps \
-
-PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl-qti \
-    android.hardware.gnss@1.0-service-qti \
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
     $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
