7c4ca7000ac67c06a203f38ce4eeee6d093dce52
Michael Bestas <mkbestas@lineageos.org>
2019-05-24 00:09:01 +0300

    cheryl: Correct double tap to wake path
    Change-Id: I80de90f66ef8792236bb114156abc323f148e812

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -172,7 +172,7 @@ TARGET_NO_RECOVERY := true
 TARGET_USES_MKE2FS := true
 
 # Power
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
+TARGET_TAP_TO_WAKE_NODE := "/proc/AllHWList/tp_double_tap"
 TARGET_USES_INTERACTION_BOOST := true
 
 # QCOM
