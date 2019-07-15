1a2f18a0c406d966082803b9af09bbfe7317efb6
Yifan Hong <elsk@google.com>
2019-07-15 16:43:26 -0700

    cheryl: Replace BOARD_CHARGER_ENABLE_SUSPEND with ro.charger.enable_suspend
    Test: charger mode
    Bug: 124118169
    Change-Id: I3825d08e38d677bd7154879609857de302348b6f

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -113,6 +113,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     camera.disable_zsl_mode=1 \
     vidc.enc.dcvs.extra-buff-count=2
 
+# Charger
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.charger.enable_suspend=true
+
 # CNE
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.cne.feature=1
