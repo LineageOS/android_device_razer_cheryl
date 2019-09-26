c21e8c216af28eee50def3312e35be322b15bd0c
Michael Bestas <mkbestas@lineageos.org>
2019-09-26 23:29:18 +0300

    cheryl: Update vendor security patch level
    * From Pie 7069
    
    Change-Id: If917fb13efd2f8ba68e1c4cbae78b9fec29a3875

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -149,7 +149,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_USES_OLD_MNC_FORMAT := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2018-07-05
+VENDOR_SECURITY_PATCH := 2019-07-05
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
