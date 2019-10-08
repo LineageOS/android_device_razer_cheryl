ad0c0e20713aee942a8e9136401dedb1623c92ae
Michael Bestas <mkbestas@lineageos.org>
2019-10-08 21:35:22 +0300

    cheryl: Build vendor variants of tinymix/tinyplay
    * Avoid SELinux neverallows, vendor services should use vendor tools
    
    Change-Id: I2a97658db9a31dd0403f1b62386db2987bd9749c

--- rootdir/bin/init.rcl.smartamp_init.sh
+++ rootdir/bin/init.rcl.smartamp_init.sh
@@ -1,9 +1,9 @@
 #!/vendor/bin/sh
 
-/system/bin/tinymix 'QUAT_MI2S_RX Audio Mixer MultiMedia5' 1 > /dev/null 2>&1
-/system/bin/tinyplay /vendor/etc/audio/silence_48k.wav -d 13 > /dev/null 2>&1 &
+/vendor/bin/tinymix 'QUAT_MI2S_RX Audio Mixer MultiMedia5' 1 > /dev/null 2>&1
+/vendor/bin/tinyplay /vendor/etc/audio/silence_48k.wav -d 13 > /dev/null 2>&1 &
 TINYPLAY_PID=$!> /dev/null 2>&1
 sleep 2
 kill $TINYPLAY_PID > /dev/null 2>&1
 sleep 0.1
-/system/bin/tinymix 'QUAT_MI2S_RX Audio Mixer MultiMedia5' 0 > /dev/null 2>&1
+/vendor/bin/tinymix 'QUAT_MI2S_RX Audio Mixer MultiMedia5' 0 > /dev/null 2>&1
