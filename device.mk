2785b90b425ad15a1bcf837a81b66fe2dd15cc37
Michael Bestas <mkbestas@lineageos.org>
2019-10-08 21:47:36 +0300

    cheryl: Remove android.hidl.base@1.0 shims
    * Patch the 2 remaining blobs that depend on it.
    * They already need libhidltransport.so so there's
      no real need for the shims anymore.
    
    Change-Id: I00f185fd4d1d8798893cf82abf9d64fcb68c3301

--- device.mk
+++ device.mk
@@ -237,11 +237,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
     android.hardware.health@2.0-service
 
-# HIDL
-PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0_system
-
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common_system
