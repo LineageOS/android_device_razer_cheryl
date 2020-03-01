c6fac6862e6d67a05e5c80f3e62aa2ad3b8774cf
Ethan Chen <intervigil@gmail.com>
2020-03-01 00:34:32 -0800

    cheryl: Remove unnecessary netutils packages
    * These are already packaged by AOSP.
    
    Change-Id: I77f91fbbb33a6d891d14fe852b094ab914ca3111

--- device.mk
+++ device.mk
@@ -298,9 +298,7 @@ PRODUCT_COPY_FILES += \
 
 # Net
 PRODUCT_PACKAGES += \
-    android.system.net.netd@1.0 \
-    android.system.net.netd@1.1.vendor \
-    libandroid_net
+    android.system.net.netd@1.1.vendor
 
 # NFC
 PRODUCT_PACKAGES += \
