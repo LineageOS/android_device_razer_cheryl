18ebaa111c88d5c5504513292037e33b56dabcef
zknb-666 <2865467965@qq.com>
2021-03-08 10:25:40 +0800

    cheryl:adapt_exthm

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_cheryl.mk
+    $(LOCAL_DIR)/exthm_cheryl.mk
--- exthm_cheryl.mk
+++ exthm_cheryl.mk
@@ -25,13 +25,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/exthm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/razer/cheryl/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_cheryl
+PRODUCT_NAME := exthm_cheryl
 PRODUCT_DEVICE := cheryl
 PRODUCT_BRAND := razer
 PRODUCT_MODEL := Phone
