0c5dc251d91c1688f606df434b37f07d5eaa264c
Jakub Pawlowski <jpawlowski@google.com>
2021-04-13 11:00:12 +0200

    cheryl: Use bluetooth.audio@2.1
    bluetooth.audio@2.1 adds new functionalities required for Bluetooth LE
    Audio development
    
    Bug: 150670922
    Change-Id: Iac2ece0c029a38fef82e81f5ba3ba491163818ef

--- device.mk
+++ device.mk
@@ -147,7 +147,7 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth.audio@2.0-impl:32 \
+    android.hardware.bluetooth.audio@2.1-impl:32 \
     audio.bluetooth.default
 
 # Boot control
