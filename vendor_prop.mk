32d325ee5db0a45e6f24dcd9da4443ab7b565a13
Michael Bestas <mkbestas@lineageos.org>
2021-09-21 01:12:26 +0300

    cheryl: Cleanup display props
    * They are set to the default value
    
    Change-Id: Ied729f8257f93c9205e619135d82a621e927778f

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -121,11 +121,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.egl.force_msaa=false \
     debug.sf.enable_gl_backpressure=1 \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
-    debug.sf.latch_unsignaled=0 \
     ro.opengles.version=196610 \
     ro.vendor.display.cabl=0 \
     vendor.display.disable_metadata_dynamic_fps=1 \
