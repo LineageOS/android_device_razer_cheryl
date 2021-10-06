f125a0abdd93abe17b8dec72956932970e772fab
TheScarastic <warabhishek@gmail.com>
2021-10-06 07:25:13 +0000

    cheryl: doze: Adapt to S style
     * Use Theme.SubSettingsBase for theme
     * Replace PreferenceActivity with CollapsingToolbarBaseActivity
     * Add Titles to prefernce screen
     * Remove onOptionsItemSelected and where neccessary move to activity
    
    Change-Id: Ic879ce61d83a33da12829008a4336c8a23230d63

--- doze/src/org/lineageos/settings/doze/PickupSensor.java
+++ doze/src/org/lineageos/settings/doze/PickupSensor.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2015 The CyanogenMod Project
- *               2017-2018 The LineageOS Project
+ * Copyright (C) 2015 The CyanogenMod Project
+ *               2017-2018,2021 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -67,7 +67,7 @@ public class PickupSensor {
         @Override
         public void onTrigger(TriggerEvent event) {
             if (DEBUG) Log.d(TAG, "Triggered");
-            Utils.wakeOrLaunchDozePulse(mContext);
+            DozeUtils.wakeOrLaunchDozePulse(mContext);
             enable();
         }
     };
