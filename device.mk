e10e49a7eaf0760e8aac5a3133a0067ad5140a9c
Bruno Martins <bgcngm@gmail.com>
2022-03-15 20:53:47 +0000

    cheryl: Copy cgroups and task_profiles directly from system/core
    This ensures that the most up-to-date JSON files are shipped.
    
    Change-Id: Ic9fae188784825ae6dc607c13b403e37290b7067

--- device.mk
+++ device.mk
@@ -177,8 +177,8 @@ PRODUCT_PACKAGES += \
 
 # Control groups and task profiles
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.4:64 \
