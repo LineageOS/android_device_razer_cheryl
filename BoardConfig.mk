d4bd0f664ca305a41df7cf7d2d7464313bb0d64b
Bruno Martins <bgcngm@gmail.com>
2022-05-30 19:27:51 +0100

    cheryl: Drop default DRM service
    This is not needed, since Widevine DRM and Clearkey DRM
    services are available.
    
    Change-Id: I97023f1bab88ddb8fa4cb205df190c516a759951

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,9 +90,6 @@ TARGET_USES_ION := true
 
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 
-# DRM
-TARGET_ENABLE_MEDIADRM_64 := true
-
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
