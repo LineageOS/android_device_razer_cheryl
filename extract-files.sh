759ed3c03e5d9514367c06d7501932fde447d397
Arian <arian.kulmer@web.de>
2023-01-03 13:02:17 +0100

    cheryl: Shorten the avoidance of duplicated added dependencies
    Change-Id: I9defe59248e537581a8ee418a1c46542f68cb5a9

--- extract-files.sh
+++ extract-files.sh
@@ -65,35 +65,23 @@ function blob_fixup() {
             sed -i 's|/product/framework/qcrilhook.jar|/system/system_ext/framework/qcrilhook.jar|g' "${2}"
             ;;
         system_ext/lib64/lib-imscamera.so)
-            for LIBGUI_SHIM in $(grep -L "libgui_shim.so" "${2}"); do
-                "${PATCHELF}" --add-needed "libgui_shim.so" "${LIBGUI_SHIM}"
-            done
+            grep -q "libgui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"
             ;;
         system_ext/lib64/lib-imsvideocodec.so)
-            for LIBGUI_SHIM in $(grep -L "libgui_shim.so" "${2}"); do
-                "${PATCHELF}" --add-needed "libgui_shim.so" "${LIBGUI_SHIM}"
-            done
-            for LIBUI_SHIM in $(grep -L "libui_shim.so" "${2}"); do
-                "${PATCHELF}" --add-needed "libui_shim.so" "$LIBUI_SHIM"
-            done
+            grep -q "libgui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"
+            grep -q "libui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
             ;;
         system_ext/lib64/libdpmframework.so)
-            for LIBDPM_SHIM in $(grep -L "libcutils_shim.so" "${2}"); do
-                "${PATCHELF}" --add-needed "libcutils_shim.so" "$LIBDPM_SHIM"
-            done
+            grep -q "libcutils_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcutils_shim.so" "${2}"
             ;;
         vendor/lib/hw/camera.msm8998.so)
             "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
             ;;
         vendor/lib/libdczoom.so)
-            for LIBUI_SHIM2 in $(grep -L "libui_shim.so" "${2}"); do
-                "${PATCHELF}" --add-needed "libui_shim.so" "$LIBUI_SHIM2"
-            done
+            grep -q "libui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
             ;;
         vendor/lib/libfusionLibrary.so)
-            for LIBUI_SHIM3 in $(grep -L "libui_shim.so" "${2}"); do
-                "${PATCHELF}" --add-needed "libui_shim.so" "$LIBUI_SHIM3"
-            done
+            grep -q "libui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
             ;;
     esac
 }
