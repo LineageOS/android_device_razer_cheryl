15d21a7295ec0959b9343019dbb148cf3178cfd6
LuK1337 <priv.luk@gmail.com>
2023-11-01 18:11:13 +0100

    cheryl: Use libutils-v33 for pm-service
    Change-Id: Ia998fa72d8747de18010abe6491d2c374cb7aa0a

--- device.mk
+++ device.mk
@@ -425,6 +425,9 @@ $(call inherit-product, build/target/product/verity.mk)
 PRODUCT_PACKAGES += \
     libstdc++_vendor
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v33.so
+
 # Wifi
 PRODUCT_PACKAGES += \
     ipacm \
