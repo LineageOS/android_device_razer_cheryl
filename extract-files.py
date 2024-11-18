a5a490cd2fcaa6411fedb9b9d62a9aeee627bb1d
Marc Bourgoin <themard85@gmail.com>
2024-11-17 17:51:59 -0700

    cheryl: Setup ELF checks & regenerate vendor
    According to templates for py extract-utils
    
    https://github.com/LineageOS/android_tools_extract-utils/tree/lineage-22.0/templates
    Change-Id: Ibb1cef7c9492c091f1626f3c4623ad0cd3e7b708

--- extract-files.py
+++ extract-files.py
@@ -12,10 +12,33 @@ from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
 )
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
+
+namespace_imports = [
+    'device/razer/cheryl',
+    'hardware/qcom-caf/msm8998',
+    'hardware/qcom-caf/wlan',
+    'vendor/qcom/opensource/dataservices',
+]
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+    ): lib_fixup_vendor_suffix,
+    (
+    ): lib_fixup_remove,
+}
 
 blob_fixups: blob_fixups_user_type = {
     (
@@ -61,8 +84,9 @@ module = ExtractUtilsModule(
     'cheryl',
     'razer',
     blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
     add_firmware_proprietary_file=True,
-    check_elf=False,
 )
 
 if __name__ == '__main__':
