d2f1192d090cfb9875d1fda96d2066f19bba6784
Michael Bestas <mkbestas@lineageos.org>
2024-12-01 22:30:22 +0200

    cheryl: gps: Convert remaining Android.mk to bp
    Change-Id: I75e6af92795fe755a48557aa06106406b14c6750

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,9 +87,6 @@ TARGET_USES_ION := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
-# GPS
-BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
-
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
--- gps/Android.mk
+++
@@ -1,7 +0,0 @@
-# Set required flags
-GNSS_CFLAGS := \
-    -Werror \
-    -Wno-undefined-bool-conversion
-
-LOCAL_PATH := $(call my-dir)
-include $(call all-makefiles-under,$(LOCAL_PATH))
