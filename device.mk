ea7ff7409c0e242dc087cc8abc3e5f4efb63d952
Michael Bestas <mkbestas@lineageos.org>
2025-04-01 19:03:51 +0300

    cheryl: Switch back to passthrough bootctrl HAL
    Change-Id: If340360ff6339b7cac1f2b090ae9e8526254cb25

--- device.mk
+++ device.mk
@@ -143,8 +143,13 @@ PRODUCT_PACKAGES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot-service.qti \
-    android.hardware.boot-service.qti.recovery
+    android.hardware.boot@1.0-impl.recovery \
+    bootctrl.msm8998.recovery
+
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl:64 \
+    android.hardware.boot@1.0-service \
+    bootctrl.msm8998 \
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
